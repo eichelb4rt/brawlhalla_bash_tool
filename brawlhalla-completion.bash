a44254d20803909cb13657269b0ff3554e27459b
eichelb4rt <maurice.wenig@gmail.com>
2025-03-20 20:39:46 +0100

    can now track random legends

--- brawlhalla-completion.bash
+++ brawlhalla-completion.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-_brawlhalla_completions()
-{
+ALL_LEGENDS="bodvar cassidy orion lord_vraxx gnash queen_nai hattori sir_roland scarlet thatch ada sentinel lucien teros brynn asuri barraza ember azoth koji ulgrim diana jhala kor wu_shang val ragnir cross mirage nix mordex yumiko artemis caspian sidra xull kaya isaiah jiro lin_fei zariel rayman dusk fait thor petra vector volkov onyx jaeyun mako magyar reno munin arcadia ezio tezca thea red_raptor loki seven vivi imugi king_zuva priya"
+
+_brawlhalla_completions() {
     CURRENT_WORD="${COMP_WORDS[COMP_CWORD]}"
     PREV_WORD="${COMP_WORDS[COMP_CWORD - 1]}"
     WORD_COUNT="${#COMP_WORDS[@]}"
@@ -12,13 +13,16 @@ _brawlhalla_completions()
     if [ "$WORD_COUNT" == "2" ]; then
         # choose sub commands
         # (brawlhalla pr -> brawlhalla profiles)
-        complete_options="profiles"
+        complete_options="profiles track"
     elif [ "$WORD_COUNT" == "3" ]; then
         # choose sub sub commands
         # (brawlhalla profiles sa -> brawlhalla profiles save)
         if [ "$SUB_COMMAND" == "profiles" ]; then
             # sub commands in profile
             complete_options="list save load delete"
+        elif [ "$SUB_COMMAND" == "track" ]; then
+            # sub commands in track
+            complete_options="random"
         fi
     elif [ "$WORD_COUNT" == "4" ]; then
         # choose sub sub command arguments
@@ -34,6 +38,11 @@ _brawlhalla_completions()
             elif [ "$SUB_SUB_COMMAND" == "delete" ]; then
                 # autocomplete to all already existing profiles
                 complete_options=$(brawlhalla profiles list)
+            fi
+        elif [ "$SUB_COMMAND" == "track" ]; then
+            if [ "$SUB_SUB_COMMAND" == "random" ]; then
+                # autocomplete to all legends
+                complete_options=$ALL_LEGENDS
             fi
         fi
     fi
--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ mkdir -p ~/.bin
 
 install ./brawlhalla ~/.bin/brawlhalla
 sudo cp brawlhalla-completion.bash /etc/bash_completion.d/brawlhalla-completion.bash
+echo "Installation complete. Please restart your terminal."
